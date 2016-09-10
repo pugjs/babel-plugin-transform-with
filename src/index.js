1efe3834aa340791d956a99655d8c83ad58ab5da
Timothy Gu <timothygu99@gmail.com>
2016-09-09 22:11:14 -0700

    Fix stray change

--- src/index.js
+++ src/index.js
@@ -129,7 +129,6 @@ export default function ({template, traverse, types: t}) {
   };
 
   return {
-    manipulateOptions: (_, parserOpts) => parserOpts.sourceType = 'script',
     visitor: {
       BlockStatement(path) {
         const {node} = path;
